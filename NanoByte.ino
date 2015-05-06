8b48d29446cd5c9e1e600c68408a5bc4aa34c58d
Andrew Nava-Juarez <anava014@ucr.edu>
2015-05-06 11:20:49 -0700

    Clarification
    Added Comments under Functions

--- NanoByte.ino
+++ NanoByte.ino
@@ -15,12 +15,6 @@
 #define RIGHT_TIMER_WITHOUT_WALL 691
 #define IN_FRONT_OF_WALL 650
 
-/*
-Currently working on Left turns.
-Too sensitive, Applyinh more confirmations (from 6 to 10)
-And lowering LEFT.. From 200 to 190
-*/
-
 int defaultErrorM = 0;
 
 int currentMillis = 0;
@@ -30,8 +24,6 @@ int randNumber = 0;
 int voidThisTravel = 0;
 int voidTime = 0;
 
-int gridCount = 0;
-
 int turnsInOneSecond = 0;
 int leftTurnClock = 0;
 
@@ -39,11 +31,7 @@ int leftLed = 22;
 int middleLed = 18;
 int rightLed = 20;
 
-int cellLED = 3;
-int speakerTimer = 0;
-int speakerIsOn = 0;
-
-bool disabled = 1; //Disable sensors at start for 1000 ms
+bool disabled = 0;
 int disabledTimer = 0;
 
 const int middleSensor = A2;
@@ -93,27 +81,23 @@ int time = 0;
 bool isTraveling = 1;
 int timeToTravel = 0;
 
+/* setup: Assign peripherals, calibrate, and move forward */
 void setup(void)
 {
-  delay(1000);
-  pinMode(leftLed, OUTPUT);
-  pinMode(middleLed, OUTPUT);
-  pinMode(rightLed, OUTPUT);
-  pinMode(cellLED, OUTPUT);
-  //gridInit();
+  
+  delay(1000);                                                                          //short delay from boot
+  pinMode(leftLed, OUTPUT); pinMode(middleLed, OUTPUT); pinMode(rightLed, OUTPUT);      //Initializing LEDS Outputs
 
   //Serial1.begin(9600);
-  myServoL.attach(9);  // Servo is connected to digital pin 9 
-  myServoR.attach(10);  // Servo is connected to digital pin 10 
-  stopServo();
-  
-  waitForOK();
-  
+
+  myServoL.attach(9); myServoR.attach(10);//stopServo();          //Initialize//  CHECK IF THIS WORKS
+
+  standBy();                                                      //Halt, Ready for calibration upon grant from owner
   calibrate();
-  time = millis();
-  forward();
+  forward();                                                      //Initiate motor rotation
 }
 
+/* fingerWave: Returns True if detected finger wave */
 int fingerWave()
 {
   if(readErrorM() > defaultErrorM + 100)
@@ -122,7 +106,8 @@ int fingerWave()
     return 0;
 }
 
-void waitForOK()
+/* standBy: Holds READY LED and waits for user */
+void standBy()
 {
   digitalWrite(middleLed, HIGH);
   defaultErrorM = readErrorM();
@@ -131,11 +116,151 @@ void waitForOK()
   delay(500);
 }
 
+/* lightShow: Controls debbugging lights */
 void lightShow() {
+
+  // waitforStop, prepringToTurnLeft, preparingToTurnRight are all booleans that get switched on once
+  // the appropriate signal is appoved.
+  // The signals preparingToTurnLeft and preparingToTurn on hold true if there are no walls in their
+  // appropriate direction
+  // waitForStop, holds true once a wall confirmed is ahead
+
   digitalWrite(middleLed, (waitForStop) ? (HIGH) : (LOW));
   digitalWrite(leftLed, (preparingToTurnLeft) ? (HIGH) : (LOW));
   digitalWrite(rightLed, (preparingToTurnRight) ? (HIGH) : (LOW));
-  
+}
+
+/* sets boolean waitForStop to true if it confirms there is a wall approaching
+The wall approaching is manially set to a distance about 8.5/10 of a cell from the wall.
+Confirmation is reached once the value is below the calibration setting a number of
+n times */
+void readFrontSensorForWall(int n){
+  errorM = readErrorM();
+  if(!waitForStop  && !disabled){                    // if its not already going to continue straight AND its NOT disabled. (meaning disabled = 0)
+    if(errorM >= WALL_APPROACHING)
+      ++frontWallApproaching;
+    else 
+      frontWallApproaching = 0;
+    if(frontWallApproaching >= n) {
+      waitForStop = 1;
+      frontWallApproaching = 0;
+    }
+  }
+  else if(waitForStop && errorM <= WALL_APPROACHING)
+    waitForStop = 0;
+}
+
+/* sets boolean readLeftSensorForWall to true if it confirms there is no wall on the left side
+The wall approaching value is set manually with careful debugging.
+Confirmation is reached once the value is below the calibration setting a number of
+n times */
+void readLeftSensorForWall(int n){                  // Overall, Checks if Left sensor detects a wall or not and raises or drops flag
+  errorL = readErrorL();
+  if(!preparingToTurnLeft && !disabled){            // if its not already going to turn left AND its NOT disabled. (meaning disabled = 0)
+    if(errorL <= LEFTWALLMISSING) {                 // There was a sudden Drop. Increment counter for Left Flag
+      ++noLeftWallApproaching;
+    }
+    else {
+      noLeftWallApproaching = 0;                    // Reset Counter
+    }
+   if(noLeftWallApproaching >= 15) {                // Confirmed there is a Missing Left Wall Approaching
+     preparingToTurnLeft = 1;                       // Raise Left Flag
+     leftClock = millis();   
+     noLeftWallApproaching = 0;                     // Reset Counter
+    }
+  }
+  else if(preparingToTurnLeft && !waitForStop && errorL >= LEFTWALLRETURNS + 100) /// CHANGE THIS SHIT ///////////////////////////////////////////////
+    preparingToTurnLeft = 0;
+}
+
+/* sets boolean readRightSensorForWall to true if it confirms there is no wall on the right side
+The wall approaching value is set manually with careful debugging.
+Confirmation is reached once the value is below the calibration setting a number of
+n times */
+void readRightSensorForWall(int n){                  // Overall, Checks if Right sensor detects a wall or not and raises or drops flag
+  errorR = readErrorR();
+  if(!preparingToTurnRight && !disabled){            // if its not already going to turn right AND its NOT disabled. (meaning disabled = 0)
+    if(errorR <= RIGHTWALLMISSING)                   // There was a sudden Drop. Increment counter for Left Flag
+      ++noRightWallApproaching;
+    else
+      noRightWallApproaching = 0;                    // Reset Counter
+      
+    if(noRightWallApproaching >= n){                 // Confirmed there is a Missing Right Wall Approaching
+      preparingToTurnRight = 1;                      // Raise Left Flag
+      rightClock = millis();
+      noRightWallApproaching = 0;                    // Reset Counter
+    }
+    
+  }
+  else{                                              // It is Preparing to turn Right, So we will check it has passed the wall to end it
+    if(!waitForStop && errorR > RIGHTWALLMISSING)    // theres no wall in front and there is a wall on the right
+      ++rightWallApproaching; 
+    else
+        rightWallApproaching = 0;
+    if(rightWallApproaching >= 5)                    // confirmation step - confirming there is a wall on the right side
+    {
+      preparingToTurnRight = 0;                      // turn flag off
+      rightWallApproaching = 0;                      // reseting the counter
+    }
+  }
+}
+
+/* At judgment of a wall with a left and right opening, choose randoly which path to take. */
+void randomTurn(){
+  randNumber = random(2);
+  if(randNumber == 0)
+    advanceLeftWithWall();
+  else
+    advanceRightWithWall();
+}
+
+/* Given the boolean left right and middle flags, We will choose whether to turn Left or Right*/
+void logic(){
+  if(waitForStop){ 
+    if(errorM >= IN_FRONT_OF_WALL) {                        // We are dead in front of a wall
+      if(preparingToTurnLeft && preparingToTurnRight)       // Both options are available, choose a random one.
+        randomTurn();
+      else if(preparingToTurnLeft)                          // Only left is available. Turn immediately
+        advanceLeftWithWall();
+      else if(preparingToTurnRight)                         // Only right is available. Turn immediately
+        advanceRightWithWall();
+      else
+        advance180Deg();                                    // None are avaiable so turn around
+
+      // Turn off all Flags
+      waitForStop = 0; 
+      preparingToTurnLeft = 0;
+      preparingToTurnRight = 0;
+
+      // Disable the left and right sensors for a given time
+      disableLeftRight();
+    }
+  }
+  else{                                                       // There is no wall in front but there might be an opening on the left or right side
+    if(preparingToTurnLeft && preparingToTurnRight)           // Both are open, forget it lets go straight
+    {}
+    else if(preparingToTurnLeft)
+      prepareForLeftTurnWithoutWall();                        // Left side is open, take it.
+//    else if(preparingToTurnRight)                           /// FIXME: Right side not ready yet.
+//      prepareForRightTurnWithoutWall();
+  }
+  enableLeftRight();                                          // After a given time, enable the left and right sensors again
+}
+
+/* Disables the left and right sensors */
+void disableLeftRight(){
+  disabled = 1;
+  disabledTimer = millis();
+}
+
+/* Enables the left and right sensors */
+void enableLeftRight(){
+  if(millis() >= disabledTimer + 460)                          //After 460 from when it was disabled, enable it
+    disabled = 0;
+}
+
+/* Prints Serial informatim */
+void debugging(){
   if(preparingToTurnLeft){
     Serial1.println("L       R       M");
     Serial1.print(errorL);
@@ -153,143 +278,14 @@ void lightShow() {
   }
 }
 
-void readFrontSensorForWall(){
-  errorM = readErrorM();
-  if(!waitForStop  && !disabled){                                 // if its not already going to continue straight AND its NOT disabled. (meaning disabled = 0)
-    if(errorM >= WALL_APPROACHING)
-      ++frontWallApproaching;
-    else 
-      frontWallApproaching = 0;
-    if(frontWallApproaching >= 6) {
-      waitForStop = 1;
-      frontWallApproaching = 0;
-    }
-  }
-  else if(waitForStop && errorM <= WALL_APPROACHING)
-    waitForStop = 0;
-}
-
-void readLeftSensorForWall(){                      //Overall, Checks if Left sensor detects a wall or not and raises or drops flag
-  errorL = readErrorL();
-  if(!preparingToTurnLeft && !disabled){            // if its not already going to turn left AND its NOT disabled. (meaning disabled = 0)
-    if(errorL <= LEFTWALLMISSING) {                 //There was a sudden Drop. Increment counter for Left Flag
-      ++noLeftWallApproaching;
-    }
-    else {
-      noLeftWallApproaching = 0;                     // Reset Counter
-    }
-   if(noLeftWallApproaching >= 15) {                  //Confirmed there is a Missing Left Wall Approaching
-     preparingToTurnLeft = 1;                        //Raise Left Flag
-     leftClock = millis();    
-     noLeftWallApproaching = 0;                      //Reset Counter
-    }
-  }
-  else if(preparingToTurnLeft && !waitForStop && errorL >= LEFTWALLRETURNS + 100)
-    preparingToTurnLeft = 0;
-}
-
-void readRightSensorForWall(){                       //Overall, Checks if Right sensor detects a wall or not and raises or drops flag
-  errorR = readErrorR();
-  if(!preparingToTurnRight && !disabled){            // if its not already going to turn right AND its NOT disabled. (meaning disabled = 0)
-    if(errorR <= RIGHTWALLMISSING)                   //There was a sudden Drop. Increment counter for Left Flag
-      ++noRightWallApproaching;
-    else
-      noRightWallApproaching = 0;                    // Reset Counter
-      
-    if(noRightWallApproaching >= 15){                 //Confirmed there is a Missing Right Wall Approaching
-      preparingToTurnRight = 1;                      //Raise Left Flag
-      rightClock = millis();
-      noRightWallApproaching = 0;                    //Reset Counter
-    }
-    
-  }
-  else{                                              // It is Preparing to turn Right, So we will check it has passed the wall to end it
-    if(!waitForStop && errorR > RIGHTWALLMISSING)    // theres no wall in front and there is a wall on the right
-      ++rightWallApproaching; 
-    else
-        rightWallApproaching = 0;
-    if(rightWallApproaching >= 5)                    // confirmation step - confirming there is a wall on the right side
-    {
-      preparingToTurnRight = 0;                      // turn flag off
-      rightWallApproaching = 0;                      // reseting the counter
-    }
-  }
-}
-
-void randomTurn(){
-  randNumber = random(2);
-  if(randNumber == 0)
-    advanceLeftWithWall();
-  else
-    advanceRightWithWall();
-}
-
-void logic(){
-  if(waitForStop){// || errorM >= IN_FRONT_OF_WALL){
-    if(errorM >= IN_FRONT_OF_WALL) {
-      if(preparingToTurnLeft && preparingToTurnRight)
-        randomTurn();
-      else if(preparingToTurnLeft)
-        advanceLeftWithWall();
-      else if(preparingToTurnRight)
-        advanceRightWithWall();
-      else
-        advance180Deg();
-
-      waitForStop = 0; 
-      preparingToTurnLeft = 0;
-      preparingToTurnRight = 0;
-      disableLeftRight();
-    }
-  }
-  else{ 
-    if(preparingToTurnLeft && preparingToTurnRight)
-    {}
-    else if(preparingToTurnLeft)
-      prepareForLeftTurnWithoutWall();
-//    else if(preparingToTurnRight)
-//      prepareForRightTurnWithoutWall();
-  }
-  enableLeftRight();
-}
-
-void disableLeftRight(){
-  disabled = 1;
-  disabledTimer = millis();
-}
-void enableLeftRight(){
-  if(millis() > disabledTimer + 460)
-    disabled = 0;
-}
-
-void turnOffCellLed(){
-  if(millis() > speakerTimer + 100)
-    digitalWrite(cellLED, LOW);
-}
-
-void turnCounter(){
-  currentMillis = millis();
-  if(turnsInOneSecond >= 2 && currentMillis < leftTurnClock + 1200){
-    time += 200;
-    turnsInOneSecond = 0;
-  }
-  else if(millis() > leftTurnClock + 1200)
-    turnsInOneSecond = 0;
-}
-
-void disableTravelVoid(){
-  if(millis() > voidTime + 150){
-    voidThisTravel = 0;
-  }
-}
-
 void loop()
 {
-  collectData();            // PID for center control
-  readFrontSensorForWall(); //Searching for Wall Approaching
-  readLeftSensorForWall(); // Searching for blank left Wall, will flag If True
-  readRightSensorForWall(); // Searching for blank right wall, will flag If True
-  
-  logic();
-  lightShow();
+  collectData();                    // PID for center control
+  readFrontSensorForWall(6);        // Searching for Wall Approaching
+  readLeftSensorForWall(15);        // Searching for blank left Wall, will flag If True
+  readRightSensorForWall(15);       // Searching for blank right wall, will flag If True
+   
+  logic();                          // Choose whether to turn left, right, or none
+  lightShow();                      // Control Debuggging lights
+  debugging();                      // Print Serial Debugging information
 }
